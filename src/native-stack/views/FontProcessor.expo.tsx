c37620f2c86b73b552fc19e8df03559ee7a0b20a
vidsag <vidhyasagar.mohanraj@gmail.com>
2020-10-19 17:53:08 +0530

    [Sagar] removed expo stuff

--- src/native-stack/views/FontProcessor.expo.tsx
+++
@@ -1,8 +0,0 @@
-// this file extension is parsed only in managed workflow, so `expo-font` should be always available there
-import { processFontFamily } from 'expo-font';
-
-export function processFonts(
-  fontFamilies: (string | undefined)[]
-): (string | undefined)[] {
-  return fontFamilies.map((fontFamily) => processFontFamily(fontFamily));
-}
